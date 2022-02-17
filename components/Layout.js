6cd178ac334f139d3b1d5646a0c91f8f3b5b5076
ig1711 <cgrecent@gmail.com>
2022-02-17 14:41:37 +0600

    sort + some ui improvements

--- components/Layout.js
+++ components/Layout.js
@@ -17,42 +17,52 @@ import AnimationContext from '../contexts/animation';
 //   `text-${m[1]}-900`,
 // ]);
 
+// stoopid tailwind doesnt work with the above in production
+
 const colorPallettes = [
   [
-    'text-emerald-100',
+    'text-emerald-100/70',
     'from-teal-900',
     'to-emerald-100',
     'shadow-[0_0_0_1px_rgb(209,250,229)_inset]',
     'rgb(209,250,229)',
     'bg-emerald-100',
     'text-teal-900',
+    'placeholder:text-emerald-100/20',
+    'rgba(6,78,59,0.3)',
   ],
   [
-    'text-cyan-100',
+    'text-cyan-100/70',
     'from-sky-900',
     'to-cyan-100',
     'shadow-[0_0_0_1px_rgb(207,250,254)_inset]',
     'rgb(207,250,254)',
     'bg-cyan-100',
     'text-sky-900',
+    'placeholder:text-cyan-100/20',
+    'rgba(12,74,110,0.3)',
   ],
   [
-    'text-pink-100',
+    'text-pink-100/70',
     'from-rose-900',
     'to-pink-100',
     'shadow-[0_0_0_1px_rgb(252,231,243)_inset]',
     'rgb(252,231,243)',
     'bg-pink-100',
     'text-rose-900',
+    'placeholder:text-pink-100/20',
+    'rgba(136,19,55,0.3)',
   ],
   [
-    'text-lime-100',
+    'text-lime-100/70',
     'from-green-900',
     'to-lime-100',
     'shadow-[0_0_0_1px_rgb(236,252,203)_inset]',
     'rgb(236,252,203)',
     'bg-lime-100',
     'text-green-900',
+    'placeholder:text-lime-100/20',
+    'rgba(20,83,45,0.3)',
   ],
 ];
 
